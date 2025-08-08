8ff70c862c9dc2dc405c094364d8109f08563fb4
prnfia <prn.fia@gmail.com>
2025-08-08 20:26:45 +0200

    “update”

--- week2/hogwarts.py
+++ week2/hogwarts.py
@@ -6,5 +6,23 @@ for i in range(len(students)):
 
 
 #2nd variation:
-students = ["Hermione", "Harry", "Ron", "Draco"]
-houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
+students = {
+    "Hermione": "Gryffindor", 
+    "Harry": "Gryffindor", 
+    "Ron": "Gryffindor", 
+    "Draco": "Slytherin",
+}
+
+for student in students:
+    print(student, students[student], sep=", ") 
+
+#3rd variation
+students = [   
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russel terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
+
+for student in students:
+    print(student["name"], students["house"], sep=", ")
