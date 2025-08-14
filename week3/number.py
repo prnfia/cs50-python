911ad8675d83765a1fd37daf87bb1bb7165a4d33
prnfia <prn.fia@gmail.com>
2025-08-14 16:15:50 +0200

    “update”

--- week3/number.py
+++ week3/number.py
@@ -15,3 +15,15 @@ main()
 
 
 #V2:
+def main():
+    x = get_int("What's x? ")
+    print (f"x is {x}")
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
+
+main()
