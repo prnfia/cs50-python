e3d8bbd33c063220551424fb7ad6872f94849fb4
prnfia <prn.fia@gmail.com>
2025-08-22 11:38:04 +0200

    “update”

--- week5. Unit Tests/calculator_test/test_calculator.py
+++ week5. Unit Tests/calculator_test/test_calculator.py
@@ -1,14 +1,22 @@
+import pytest 
+
 from calculator import square 
+
 
 def test_positive():
     assert square(2) == 4
     assert square(3) == 9
 
+
 def test_negative():
     assert square(-2) == 4
     assert square(-3) == 9
-    
+
+
 def test_zero():
     assert square(0) == 0
 
-# pytest test_calculator.py
+
+def test_str():
+    with pytest.raises(TypeError):
+        square("cat")
