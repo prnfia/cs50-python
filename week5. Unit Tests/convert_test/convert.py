8e63fcdfd4d230ab0d7dc1f6c276f43e5c83630d
prnfia <prn.fia@gmail.com>
2025-08-22 11:35:17 +0200

    “update”

--- week5. Unit Tests/convert_test/convert.py
+++ week5. Unit Tests/convert_test/convert.py
@@ -9,10 +9,12 @@ def main():
 
     print(f"{au} AU is {convert(au)} m")
 
+
 def convert(au):
     if not isinstance(au, (int, float)):
         raise TypeError("au must be an int or float")
     return au * 149597870700
+
 
 if __name__ == "__main__":
     main() 
